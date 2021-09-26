e69ca8962275a2aa12d7dd8212477b98a2f15da9
SHI Wenxuan <whexy@outlook.com>
2021-09-26 17:15:28 +0800

    [feature] read_etm_reg

--- src/etm/reg.rs
+++ src/etm/reg.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::{error::Error, fs::File, io::Read};
 
 use super::device::Device;
 
@@ -7,11 +7,19 @@ pub struct EtmReg {
     cpu: u8,
 }
 
-pub fn get_device_info(d: &Device) -> EtmReg {
-    let path = format!("{}/cpu", d.sysfs);
-    let mut file = std::fs::File::open(path).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let cpu: u8 = contents.as_str().trim().parse().unwrap();
-    EtmReg { cpu }
+/// read ETM reg from sysfs
+fn read_etm_reg(
+    d: &Device,
+    reg: &str,
+) -> Result<String, Box<dyn Error>> {
+    let path = format!("{}/{}", d.sysfs, reg);
+    let mut file = File::open(path)?;
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer)?;
+    Ok(buffer.trim().to_string())
+}
+
+pub fn get_device_info(d: &Device) -> Result<EtmReg, Box<dyn Error>> {
+    let cpu: u8 = read_etm_reg(d, "cpu")?.parse()?;
+    Ok(EtmReg { cpu })
 }
